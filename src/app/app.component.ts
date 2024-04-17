3797df3f549bdf5cf03777777effea77f2ac8d0c
Aditira Jamhuri <aditirajamhuri@gmail.com>
2024-04-17 09:02:31 +0700

    feat: add user bosses list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './components/user/user.component';
 
 @Component({
-  selector: 'app-root',
+  selector: 'main-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'Aditira Tamvan';
 }
