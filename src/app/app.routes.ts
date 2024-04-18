3c453d55b96b408bbd39368849969266abdac3c8
Aditira Jamhuri <aditirajamhuri@gmail.com>
2024-04-18 10:44:17 +0700

    feat: add form module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,20 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UserComponent } from './components/user/user.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        title: 'App Login Page',
+        component: LoginComponent
+    },
     {
         path: 'home',
         title: 'App Home Page',
@@ -12,5 +24,10 @@ export const routes: Routes = [
         path: 'user',
         title: 'App User Page',
         component: UserComponent
+    },
+    {
+        path: '**',
+        title: 'Page Not Found',
+        component: NotfoundComponent
     }
 ];
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]),
+    password: new FormControl('', null)
+  })
+
+  ngOnInit() {
+    console.log(this.loginForm.value);
+  }
+
+  login() {
+    if (!this.loginForm.valid) {
+      // alert('Form is not valid');
+      console.log(this.loginForm);
+      return;
+    }
+
+    alert(
+      JSON.stringify(this.loginForm.value)
+    )
+  }
+}
