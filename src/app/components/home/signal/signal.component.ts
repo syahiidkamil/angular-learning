d0da39bf8903274b846c38a0fa9d0f2195f43f6e
Aditira Jamhuri <aditirajamhuri@gmail.com>
2024-04-29 10:15:25 +0700

    feat: add practice signal

--- src/app/components/home/signal/signal.component.ts
+++ src/app/components/home/signal/signal.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, signal } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 import { SignalService } from '../../../services/signal.service';
 
 @Component({
@@ -9,14 +9,47 @@ import { SignalService } from '../../../services/signal.service';
   styleUrl: './signal.component.scss'
 })
 export class SignalComponent {
+  readonly count = signal(0);
 
   constructor(private signalService: SignalService) {
-      let count = signalService.updateCount();
- 
       // Register a new effect.
       effect(() => {
-        console.log(`The count is: ${count}`);
+        console.log(`The count is: ${"Coba kau bayangkan coba kau renungkan"}.`);
       });
+  }
+
+  ngOnInit() {
+    setInterval(() => {
+      this.count.set(this.count() + 1);
+    }, 1000);
+  }
+
+  calculateNumber() {
+    // let a = 10;
+    // let b = 20;
+    // let c = a + b;
+    // console.log(c);
+    // a = 20;
+    // console.log(c);
+
+    // const a = signal(10);
+    // const b = signal(20);
+    // const c = computed(() => a() + b());
+    // console.log(c());
+    // a.set(100);
+    // console.log(c());
+
+    // const showCount = signal(true);
+    // const count = signal(0);
+    // const conditionalCount = computed(() => {
+    //   if (showCount()) {
+    //     return `The count is ${count()}.`;
+    //   } else {
+    //     return 'Nothing to see here!';
+    //   }
+    // });
+
+    // console.log(conditionalCount());
   }
   
   
