b64b82aef1ea72840df400e69fe429e7d3f2ce14
Aditira Jamhuri <aditirajamhuri@gmail.com>
2024-04-29 15:43:48 +0700

    feat: init cart feature in signal

--- src/app/components/home/signal/signal.component.ts
+++ src/app/components/home/signal/signal.component.ts
@@ -1,57 +1,106 @@
 import { Component, computed, effect, signal } from '@angular/core';
-import { SignalService } from '../../../services/signal.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+export interface Coffee {
+  id: number;
+  name: string;
+  price: number;
+  stock: number;
+}
+
+export interface CoffeeCart {
+  id: number;
+  name: string;
+  price: number;
+  stock: number;
+  quantity: number;
+}
 
 @Component({
   selector: 'app-signal',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './signal.component.html',
   styleUrl: './signal.component.scss'
 })
 export class SignalComponent {
-  readonly count = signal(0);
+  coffees: Coffee[] = [
+    {
+      id: 1,
+      name: "Cappuccino",
+      price: 50000,
+      stock: 10
+    },
+    {
+      id: 2,
+      name: "Espresso",
+      price: 1000000,
+      stock: 3
+    },
+    {
+      id: 3,
+      name: "Luwak",
+      price: 5000,
+      stock: 50
+    },
+    {
+      id: 4,
+      name: "Latte",
+      price: 10000,
+      stock: 0
+    }
+  ]
 
-  constructor(private signalService: SignalService) {
-      // Register a new effect.
-      effect(() => {
-        console.log(`The count is: ${"Coba kau bayangkan coba kau renungkan"}.`);
-      });
+  cartCoffees: CoffeeCart[] = [];
+
+  quantity = signal<number>(1);
+  selectedCoffee = signal<Coffee>(this.coffees[0]);
+  numbers = [1, 2, 3, 4, 5];
+
+  totalPrice = computed(() => {
+    return this.quantity() * this.selectedCoffee().price
+  })
+
+  addCart(coffee: Coffee) {
+    // Check stock
+    if (coffee.stock == 0) {
+      alert('Out of Stock');
+      return
+    }
+
+    // Create new coffee
+    let newCooffee = {...coffee, quantity: 1};
+
+
+    // Check if already in cart
+    if (this.cartCoffees.find(x => x.id == newCooffee.id)) {
+      alert('Already in cart');
+      return
+    }
+
+    // Add to cart
+    this.cartCoffees.push(newCooffee);
+  }
+
+  changeQuantity(index: number, idCart: number, quantity: string) {
+    this.cartCoffees[index].quantity = Number(quantity);
+    // this.coffees[].stock = 0
+
+    console.log(this.cartCoffees);
+  }
+
+
+  constructor() {
+      
   }
 
   ngOnInit() {
-    setInterval(() => {
-      this.count.set(this.count() + 1);
-    }, 1000);
+    
   }
 
-  calculateNumber() {
-    // let a = 10;
-    // let b = 20;
-    // let c = a + b;
-    // console.log(c);
-    // a = 20;
-    // console.log(c);
+  buy() {
 
-    // const a = signal(10);
-    // const b = signal(20);
-    // const c = computed(() => a() + b());
-    // console.log(c());
-    // a.set(100);
-    // console.log(c());
-
-    // const showCount = signal(true);
-    // const count = signal(0);
-    // const conditionalCount = computed(() => {
-    //   if (showCount()) {
-    //     return `The count is ${count()}.`;
-    //   } else {
-    //     return 'Nothing to see here!';
-    //   }
-    // });
-
-    // console.log(conditionalCount());
   }
   
-  
-
 }
