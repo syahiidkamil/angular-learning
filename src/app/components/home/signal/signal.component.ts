ac81efc265057f7b36fc2f06d82865e1c5d35aba
Aditira Jamhuri <aditirajamhuri@gmail.com>
2024-05-01 18:11:02 +0700

    feat: fix coffe cart order with signal

--- src/app/components/home/signal/signal.component.ts
+++ src/app/components/home/signal/signal.component.ts
@@ -1,5 +1,5 @@
-import { Component, computed, effect, signal } from '@angular/core';
-import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component, Signal, computed, effect, inject, signal } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 export interface Coffee {
@@ -14,7 +14,6 @@ export interface CoffeeCart extends Coffee {
   totalPrice: number;
   initialStock: number;
 }
-
 
 export interface User {
   id: number;
@@ -31,36 +30,37 @@ export interface User {
   styleUrl: './signal.component.scss'
 })
 export class SignalComponent {
-  coffees: Coffee[] = [
-    {
-      id: 1,
-      name: "Cappuccino",
-      price: 50000,
-      stock: 10
-    },
-    {
-      id: 2,
-      name: "Espresso",
-      price: 1000000,
-      stock: 3
-    },
-    {
-      id: 3,
-      name: "Luwak",
-      price: 5000,
-      stock: 50
-    },
-    {
-      id: 4,
-      name: "Latte",
-      price: 10000,
-      stock: 0
-    }
-  ]
+  coffees = signal<Coffee[]>(
+    [
+      {
+        id: 1,
+        name: "Cappuccino",
+        price: 50000,
+        stock: 10
+      },
+      {
+        id: 2,
+        name: "Espresso",
+        price: 1000000,
+        stock: 3
+      },
+      {
+        id: 3,
+        name: "Luwak",
+        price: 5000,
+        stock: 50
+      },
+      {
+        id: 4,
+        name: "Latte",
+        price: 10000,
+        stock: 0
+      }
+    ]
+)
 
-  cartCoffees: CoffeeCart[] = [];
-
-  totalPrice: number = 0;
+  cartCoffees = signal<CoffeeCart[]>([]);
+  totalPrice = computed(() => this.cartCoffees().reduce((a, b) => a + b.totalPrice, 0));  
 
   addCart(indexCoffee: number, coffee: Coffee) {
     // Check stock
@@ -70,23 +70,20 @@ export class SignalComponent {
     }
 
     // Create new coffee
-    let newCooffee = { ...coffee, quantity: 1, totalPrice: coffee.price, initialStock: coffee.stock };
+    let newCooffee = signal<CoffeeCart>({ ...coffee, quantity: 1, totalPrice: coffee.price, initialStock: coffee.stock });
 
 
     // Check if already in cart
-    if (this.cartCoffees.find(x => x.id == newCooffee.id)) {
+    if (this.cartCoffees().find(x => x.id == newCooffee().id)) {
       alert('Already in cart');
       return
     }
 
     // Add to cart
-    this.cartCoffees.push(newCooffee);
+    this.cartCoffees.set([...this.cartCoffees(), newCooffee()]);
 
     // Reduce stock
-    this.coffees[indexCoffee].stock -= 1
-
-    // Calculate total
-    this.totalPrice = this.cartCoffees.reduce((a, b) => a + b.totalPrice, 0)
+    this.coffees()[indexCoffee].stock -= 1;
   }
 
   changeQuantity(index: number, idCart: number, value: string) {
@@ -98,25 +95,29 @@ export class SignalComponent {
     }
 
     // Check if quantity is over stock, then set to stock amount
-    let indexCoffee = this.coffees.findIndex(x => x.id == idCart);
-    if (quantity > this.cartCoffees[index].initialStock) {
-      quantity = this.cartCoffees[index].initialStock;
+    let indexCoffee = this.coffees().findIndex(x => x.id == idCart);
+    if (quantity > this.cartCoffees()[index].initialStock) {
+      quantity = this.cartCoffees()[index].initialStock;
     }
 
     // Change quantity
-    this.cartCoffees[index].quantity = quantity;
-    this.cartCoffees[index].totalPrice = quantity * this.coffees[indexCoffee].price;
-    this.coffees[indexCoffee].stock = this.cartCoffees[index].initialStock - quantity;
-
-    // Calculate total
-    this.totalPrice = this.cartCoffees.reduce((a, b) => a + b.totalPrice, 0)
+    this.cartCoffees()[index].quantity = quantity;
+    this.cartCoffees.update(data => {
+      return data.map(x => {
+        if (x.id == idCart) {
+          return { ...x, totalPrice: x.price * quantity }
+        }
+        return x
+      })
+    });
+    this.coffees()[indexCoffee].stock = this.cartCoffees()[index].initialStock - quantity;
   }
 
   removeCart(index: number, coffee: CoffeeCart) {
     // Remove from cart
-    this.cartCoffees.splice(index, 1);
+    this.cartCoffees().splice(index, 1);
     // Increase stock
-    this.coffees[coffee.id - 1].stock += coffee.quantity
+    this.coffees()[coffee.id - 1].stock += coffee.quantity
   }
 
   constructor() {
@@ -124,7 +125,10 @@ export class SignalComponent {
   }
 
   ngOnInit() {
-
+    effect(() =>{
+      console.log(this.totalPrice);
+      console.log(this.cartCoffees);
+      console.log(this.coffees);
+    });
   }
-
 }
